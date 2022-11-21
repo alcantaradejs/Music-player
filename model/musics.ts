c48299ddff78b008273ee5b5abea9bb66c5b1a60
Batatinha <batatinha@pop-os.localdomain>
2022-11-21 19:28:00 -0300

    create:music model

---
+++ model/musics.ts
@@ -0,0 +1,21 @@
+export type MusicType = {
+	title: string
+	artist: string
+	coverURL: string
+	URL: string
+}
+
+export const MusicList : MusicType[] = [
+	{
+		title: "Musica de teste 1",
+		artist: "Batatatinha",
+		coverURL: "https://images.unsplash.com/photo-1468164016595-6108e4c60c8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+		URL: "#",
+	},
+	{
+		title: "Musica de teste 2",
+		artist: "Batatatinha",
+		coverURL: "https://images.unsplash.com/photo-1468164016595-6108e4c60c8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+		URL: "#",
+	}
+]
